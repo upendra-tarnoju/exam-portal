c9abd6c773fe22a833659d8ac2163b1f61e8452a
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-08-03 15:10:53 +0530

    created node js server

---
+++ backend/config/server.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+var corsOptions = {
+	origin: 'http://localhost:3000',
+	optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+dotenv.config();
+const { HOST, PORT } = require('./config');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: true },
+	})
+);
+
+app.listen(PORT, HOST, (err) => {
+	if (err) console.log(err);
+	else console.log(`Running on ${HOST}:${PORT}`);
+});
