9ba353e07b3097014b4d573e2a9e4874c667fe08
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-08-04 08:33:20 +0530

    added models and routes

---
+++ backend/routes/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { userController } = require('../controllers');
+
+module.exports = () => {
+	const router = express.Router();
+
+	router.post('/signup', userController.saveUserDetails);
+
+	return router;
+};
