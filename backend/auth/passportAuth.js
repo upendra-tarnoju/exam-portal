737822d01c7f16abbfa3c8f2a1552d50ce441f17
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-08-07 16:35:26 +0530

    configured passport auth

---
+++ backend/auth/passportAuth.js
@@ -0,0 +1,32 @@
+const { users } = require('../models');
+const userSchema = require('../schemas').users;
+const LocalStrategy = require('passport-local').Strategy;
+const bcryptjs = require('bcryptjs');
+
+module.exports = (passport) => {
+	passport.use(
+		new LocalStrategy((email, password, done) => {
+			users
+				.find(email)
+				.select({ email: 1, accountType: 1, accountStatus: 1, password: 1 })
+				.then((user) => {
+					let userStatus = bcryptjs.compareSync(password, user.password);
+					if (userStatus) return done(null, user);
+					else
+						return done(null, false, {
+							message: 'Incorrect credentials',
+						});
+				});
+		})
+	);
+
+	passport.serializeUser((user, done) => {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser((id, done) => {
+		userSchema.findById(id, (err, user) => {
+			done(err, user);
+		});
+	});
+};
