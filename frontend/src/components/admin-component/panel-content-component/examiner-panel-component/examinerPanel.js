e9396a195d140b576d944f02ea377644f2ef89a1
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-08-12 12:51:03 +0530

    show pending examiner data with logic done

--- frontend/src/components/admin-component/panel-content-component/examiner-panel-component/examinerPanel.js
+++ frontend/src/components/admin-component/panel-content-component/examiner-panel-component/examinerPanel.js
@@ -1,7 +1,77 @@
 import React, { Component } from 'react';
 import styles from './examinerPanel.module.css';
+import axios from 'axios';
 
 class ExaminerPanel extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tableData: [],
+		};
+		this.examinerData = this.examinerData.bind(this);
+	}
+
+	handleCardClick(type) {
+		axios
+			.get(`${process.env.REACT_APP_BASE_URL}/api/admin/examiner`, {
+				params: { type: type },
+			})
+			.then((res) => {
+				if (res.data.length !== 0) {
+					this.setState({
+						tableData: res.data,
+					});
+				}
+			});
+	}
+
+	examinerData() {
+		let examiners = this.state.tableData.map((data, index) => (
+			<tr key={data._id}>
+				<th scope='row'>{index + 1}</th>
+				<td>{data.firstName}</td>
+				<td>{data.lastName}</td>
+				<td>{data.email}</td>
+				<td>
+					<button
+						type='button'
+						className={`${styles.icon} btn p-0`}
+						data-toggle='tooltip'
+						data-placement='top'
+						title='Decline'
+					>
+						<i className={'fa fa-trash-o cursor-pointer text-white'}></i>
+					</button>
+					<button
+						type='button'
+						className={`${styles.icon} btn pr-2`}
+						data-toggle='tooltip'
+						data-placement='top'
+						title='Approve'
+					>
+						<i className='fa fa-check-square-o cursor-pointer text-white'></i>
+					</button>
+				</td>
+			</tr>
+		));
+		return (
+			<div className='mt-4 table-responsive'>
+				<table className='table table-hover table-dark'>
+					<thead>
+						<tr>
+							<th scope='col'>S.No</th>
+							<th scope='col'>First name</th>
+							<th scope='col'>Last name</th>
+							<th scope='col'>Email</th>
+							<th scope='col'>Actions</th>
+						</tr>
+					</thead>
+					<tbody>{examiners}</tbody>
+				</table>
+			</div>
+		);
+	}
+
 	render() {
 		return (
 			<div className={`container pt-4`}>
@@ -34,6 +104,7 @@ class ExaminerPanel extends Component {
 					<div className='col-md-4'>
 						<div
 							className={`card p-3 ${styles.pendingCard} cursor-pointer ${styles.iconHover}`}
+							onClick={() => this.handleCardClick('pending')}
 						>
 							<img
 								alt='pending icon'
@@ -82,6 +153,7 @@ class ExaminerPanel extends Component {
 						</div>
 					</div>
 				</div>
+				{this.state.tableData.length !== 0 ? <this.examinerData /> : null}
 			</div>
 		);
 	}
