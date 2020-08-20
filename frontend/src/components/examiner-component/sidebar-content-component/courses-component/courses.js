ce2010ea4f25aed4a71c9c3f40172cf5ea617e86
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-08-20 10:06:03 +0530

    updated courses component

--- frontend/src/components/examiner-component/sidebar-content-component/courses-component/courses.js
+++ frontend/src/components/examiner-component/sidebar-content-component/courses-component/courses.js
@@ -153,59 +153,50 @@ class Courses extends Component {
 				>
 					{this.state.msg}
 				</Alert>
-				<div className='card'>
-					<div className={`card-header text-center ${styles.heading}`}>
-						Courses
+				<div className={`text-center ${styles.heading} mb-2`}>Courses</div>
+				{this.state.courses.length === 0 ? (
+					<p className='mb-0'>No course existed. Create a new one </p>
+				) : (
+					<div className='table-responsive'>
+						<table className='table table-hover table-dark mb-0'>
+							<thead>
+								<tr>
+									<th scope='col'>S.No</th>
+									<th scope='col'>Name</th>
+									<th scope='col'>Description</th>
+									<th scope='col'>Actions</th>
+								</tr>
+							</thead>
+							{courses}
+						</table>
+						<div className='py-2 px-1 bg-light d-flex justify-content-end'>
+							<span className='align-self-center mr-3'>
+								Item per page
+							</span>
+							<select
+								onChange={this.changePageSize}
+								value={this.state.pageSize}
+								className={`form-control ${styles.selectWidth} form-control-sm mr-3`}
+							>
+								<option>5</option>
+								<option>10</option>
+								<option>15</option>
+							</select>
+							<span className='align-self-center mr-3'>
+								{this.state.tableIndex + 1} - {this.state.maxSizeIndex}{' '}
+								of {this.state.totalCount}
+							</span>
+							<i
+								onClick={() => this.paginateCourses('dec')}
+								className='fa fa-2x fa-angle-left align-self-center mr-3 cursor-pointer'
+							></i>
+							<i
+								onClick={() => this.paginateCourses('inc')}
+								className='fa fa-2x fa-angle-right align-self-center cursor-pointer'
+							></i>
+						</div>
 					</div>
-					<div className='card-body'>
-						{this.state.courses.length === 0 ? (
-							<p className='mb-0'>
-								No course existed. Create a new one{' '}
-							</p>
-						) : (
-							<div className='table-responsive'>
-								<table className='table table-hover table-dark mb-0'>
-									<thead>
-										<tr>
-											<th scope='col'>S.No</th>
-											<th scope='col'>Name</th>
-											<th scope='col'>Description</th>
-											<th scope='col'>Actions</th>
-										</tr>
-									</thead>
-									{courses}
-								</table>
-								<div className='py-2 px-1 bg-light d-flex justify-content-end'>
-									<span className='align-self-center mr-3'>
-										Item per page
-									</span>
-									<select
-										onChange={this.changePageSize}
-										value={this.state.pageSize}
-										className={`form-control ${styles.selectWidth} form-control-sm mr-3`}
-									>
-										<option>5</option>
-										<option>10</option>
-										<option>15</option>
-									</select>
-									<span className='align-self-center mr-3'>
-										{this.state.tableIndex + 1} -{' '}
-										{this.state.maxSizeIndex} of{' '}
-										{this.state.totalCount}
-									</span>
-									<i
-										onClick={() => this.paginateCourses('dec')}
-										className='fa fa-2x fa-angle-left align-self-center mr-3 cursor-pointer'
-									></i>
-									<i
-										onClick={() => this.paginateCourses('inc')}
-										className='fa fa-2x fa-angle-right align-self-center cursor-pointer'
-									></i>
-								</div>
-							</div>
-						)}
-					</div>
-				</div>
+				)}
 				<CreateCourses
 					show={this.state.create}
 					closeModal={this.handleCreate}
