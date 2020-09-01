82167650dfbc5b122f9876f8044b91841041a5ed
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-09-01 21:45:52 +0530

    refactored exam code

---
+++ frontend/src/components/examiner-component/sidebar-content-component/exam-component/examTable.js
@@ -0,0 +1,210 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+import moment from 'moment';
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+import * as ActionTypes from '../../../../action';
+
+class ExamTable extends Component {
+	constructor(props) {
+		super(props);
+		this.handleExamChange = this.handleExamChange.bind(this);
+	}
+
+	editExam(status, index) {
+		this.props.editExam(status, index);
+		if (status) {
+			this.props.setExamInputs(this.props.examsList[index]);
+		}
+	}
+
+	handleExamChange(event) {
+		let update = false;
+		let key = event.target.name;
+		let value = event.target.value;
+		if (key === 'totalMarks' || key === 'passingMarks') {
+			let letters = /^[0-9\b]+$/;
+			if (letters.test(value) || value === '') {
+				update = true;
+			}
+		} else {
+			update = true;
+		}
+		if (update) {
+			this.props.updateExamInputs(key, value);
+		}
+	}
+
+	render() {
+		let { exam, index, selectedExamIndex, editExam, input } = this.props;
+		return (
+			<tr key={exam._id}>
+				<td>{index + 1}</td>
+				<td>
+					{selectedExamIndex === index && editExam ? (
+						<input
+							type='text'
+							name='subject'
+							className='w-100 form-control'
+							value={input.subject}
+							onChange={this.handleExamChange}
+						/>
+					) : (
+						exam.subject
+					)}
+				</td>
+				<td>
+					{selectedExamIndex === index && editExam ? (
+						<input
+							type='text'
+							name='examCode'
+							className='w-100 form-control'
+							value={input.examCode}
+							onChange={this.handleExamChange}
+						/>
+					) : (
+						exam.examCode
+					)}
+				</td>
+				<td>
+					{selectedExamIndex === index && editExam ? (
+						<input
+							type='date'
+							name='examDate'
+							value={moment(input.examDate).format('YYYY-MM-DD')}
+							onChange={this.handleExamChange}
+							className='w-100 form-control'
+						/>
+					) : (
+						<Moment format='MMM Do, YYYY'>{exam.examDate}</Moment>
+					)}
+				</td>
+				<td>
+					{selectedExamIndex === index && editExam ? (
+						<input
+							type='text'
+							name='totalMarks'
+							value={input.totalMarks}
+							onChange={this.handleExamChange}
+							className='w-100 form-control text-right'
+						/>
+					) : (
+						<div className='text-right'>{exam.totalMarks}</div>
+					)}
+				</td>
+				<td>
+					{selectedExamIndex === index && editExam ? (
+						<input
+							type='text'
+							name='passingMarks'
+							value={input.passingMarks}
+							onChange={this.handleExamChange}
+							className='w-100 form-control text-right'
+						/>
+					) : (
+						<div className='text-right'>{exam.passingMarks}</div>
+					)}
+				</td>
+				<td>
+					{selectedExamIndex === index && editExam ? (
+						<input
+							type='time'
+							name='startTime'
+							value={moment(input.startTime).format('HH:MM')}
+							onChange={this.handleExamChange}
+							className='w-100 form-control'
+						/>
+					) : (
+						<Moment format='HH:mm A'>{exam.startTime}</Moment>
+					)}
+				</td>
+				<td>
+					{selectedExamIndex === index && editExam ? (
+						<input
+							type='time'
+							name='endTime'
+							value={moment(input.endTime).format('HH:MM')}
+							onChange={this.handleExamChange}
+							className='w-100 form-control'
+						/>
+					) : (
+						<Moment format='HH:mm A'>{exam.endTime}</Moment>
+					)}
+				</td>
+				<td className='d-flex justify-content-around'>
+					<OverlayTrigger
+						placement='bottom'
+						overlay={<Tooltip id='button-tooltip'>Delete</Tooltip>}
+					>
+						<i className='fa fa-trash-o cursor-pointer text-white align-self-center'></i>
+					</OverlayTrigger>
+					<OverlayTrigger
+						placement='bottom'
+						overlay={<Tooltip id='button-tooltip'>Add Questions</Tooltip>}
+					>
+						<i className='fa fa-plus cursor-pointer text-white align-self-center'></i>
+					</OverlayTrigger>
+					{selectedExamIndex === index && editExam ? (
+						<OverlayTrigger
+							placement='bottom'
+							overlay={<Tooltip id='button-tooltip'>Cancel</Tooltip>}
+						>
+							<i
+								className='fa fa-times cursor-pointer text-white align-self-center ml-1 mr-1'
+								onClick={() => this.editExam(false, '')}
+							></i>
+						</OverlayTrigger>
+					) : (
+						<OverlayTrigger
+							placement='bottom'
+							overlay={
+								<Tooltip id='button-tooltip'>Update exam</Tooltip>
+							}
+						>
+							<i
+								className='fa fa-check-square-o cursor-pointer text-white align-self-center'
+								onClick={() => this.editExam(true, index)}
+							></i>
+						</OverlayTrigger>
+					)}
+				</td>
+			</tr>
+		);
+	}
+}
+
+const mapStateToProps = (state) => {
+	return {
+		editExam: state.examReducer.editExam,
+		selectedExamIndex: state.examReducer.selectedExamIndex,
+		examsList: state.examReducer.examsList,
+		input: state.examReducer.editExamInputs,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		editExam: (status, index) => {
+			dispatch({
+				type: ActionTypes.EDIT_EXAM_STATUS,
+				status: status,
+				index: index,
+			});
+		},
+		updateExamInputs: (key, value) => {
+			dispatch({
+				type: ActionTypes.EDIT_EXAM_INPUTS,
+				key: key,
+				value: value,
+			});
+		},
+		setExamInputs: (inputs) => {
+			dispatch({
+				type: ActionTypes.SET_EXAM_INPUTS,
+				inputs: inputs,
+			});
+		},
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExamTable);
--- frontend/src/reducers/examReducer.js
+++ frontend/src/reducers/examReducer.js
@@ -1,4 +1,7 @@
 const initialState = {
+	editExam: false,
+	selectedExamIndex: '',
+	examsList: [],
 	examDetails: {
 		subject: '',
 		course: '',
@@ -10,6 +13,15 @@ const initialState = {
 			examCode: '',
 			password: '',
 		},
+	},
+	editExamInputs: {
+		subject: '',
+		examCode: '',
+		examDate: '',
+		totalMarks: '',
+		passingMarks: '',
+		startTime: '',
+		endTime: '',
 	},
 };
 
@@ -40,6 +52,30 @@ const examReducers = (state = initialState, action) => {
 				password: '',
 				subject: '',
 			},
+		};
+	} else if (action.type === 'edit_exam_status') {
+		return {
+			...state,
+			editExam: action.status,
+			selectedExamIndex: action.index,
+		};
+	} else if (action.type === 'set_exam_list') {
+		return {
+			...state,
+			examsList: action.examList,
+		};
+	} else if (action.type === 'edit_exam_inputs') {
+		return {
+			...state,
+			editExamInputs: {
+				...state.editExamInputs,
+				[action.key]: action.value,
+			},
+		};
+	} else if (action.type === 'set_exam_inputs') {
+		return {
+			...state,
+			editExamInputs: action.inputs,
 		};
 	}
 	return state;
