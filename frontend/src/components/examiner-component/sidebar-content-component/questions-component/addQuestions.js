31c149910a73a47fea1abd2e1f22553a0b69e604
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-09-27 19:09:01 +0530

    fixed route error

--- frontend/src/components/examiner-component/sidebar-content-component/questions-component/addQuestions.js
+++ frontend/src/components/examiner-component/sidebar-content-component/questions-component/addQuestions.js
@@ -179,7 +179,7 @@ class AddQuestions extends React.Component {
 	}
 
 	editExam(pathname) {
-		if (!pathname.endsWith('question') && !pathname.endsWith('exam')) {
+		if (!pathname.endsWith('question/new') && !pathname.endsWith('exam')) {
 			let questionId = pathname.split('/question/')[1];
 			this.questionService.getParticular(questionId).then((response) => {
 				this.setValues(response.data);
